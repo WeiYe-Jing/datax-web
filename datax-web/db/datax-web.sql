599f76edf40aae45695f9f7e444693f1f678eec3
zhouhongfa <zhouhongfa@gz-yibo.com>
2019-05-20 10:31:54 +0800

    update sql file

--- datax-web/db/datax-web.sql
+++ datax-web/db/datax-web.sql
@@ -18,31 +18,6 @@ SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for datax_content
--- ----------------------------
-DROP TABLE IF EXISTS `datax_content`;
-CREATE TABLE `datax_content`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `plugin_type` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '插件类型，区分reader writer',
-  `params_json` text CHARACTER SET utf8 COLLATE utf8_general_ci COMMENT '插件配置参数json字符串',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = 'datax job content 信息' ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for datax_job
--- ----------------------------
-DROP TABLE IF EXISTS `datax_job`;
-CREATE TABLE `datax_job`  (
-  `id` int(11) NOT NULL COMMENT '主键',
-  `job_id` int(11) DEFAULT NULL COMMENT '记录datax job id',
-  `setting_json` text CHARACTER SET utf8 COLLATE utf8_general_ci COMMENT 'setting json字符串',
-  `content_ids` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '关联datax_content表，用逗号分隔',
-  `job_log_path` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '记录日志文件路径',
-  `extra_json` text CHARACTER SET utf8 COLLATE utf8_general_ci COMMENT '除了setting content外的一些配置',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = 'data job配置' ROW_FORMAT = Dynamic;
-
--- ----------------------------
 -- Table structure for datax_plugin
 -- ----------------------------
 DROP TABLE IF EXISTS `datax_plugin`;

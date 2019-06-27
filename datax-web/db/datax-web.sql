fb5ba36f14102b21c2d54d3f65e7868ff3b78055
zhouhongfa <zhouhongfa@gz-yibo.com>
2019-06-27 21:00:08 +0800

    update sql backup file

--- datax-web/db/datax-web.sql
+++ datax-web/db/datax-web.sql
@@ -1,83 +1,97 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : 192.168.1.189_3306
-Source Server Version : 50724
-Source Host           : 192.168.1.189:3306
-Source Database       : datax-web
+ Source Server         : 192.168.1.150-3306
+ Source Server Type    : MySQL
+ Source Server Version : 50723
+ Source Host           : 192.168.1.150:3306
+ Source Schema         : datax-web
 
-Target Server Type    : MYSQL
-Target Server Version : 50724
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 50723
+ File Encoding         : 65001
 
-Date: 2019-06-17 22:50:00
+ Date: 27/06/2019 20:59:03
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for datax_plugin
 -- ----------------------------
 DROP TABLE IF EXISTS `datax_plugin`;
-CREATE TABLE `datax_plugin` (
+CREATE TABLE `datax_plugin`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `plugin_type` varchar(32) DEFAULT NULL COMMENT '插件类型，reader writer',
-  `plugin_name` varchar(255) NOT NULL COMMENT '插件名，用作主键',
-  `template_json` text COMMENT 'json模板',
-  `comments` varchar(1000) DEFAULT NULL COMMENT '注释',
+  `plugin_type` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '插件类型，reader writer',
+  `plugin_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '插件名，用作主键',
+  `template_json` text CHARACTER SET utf8 COLLATE utf8_general_ci COMMENT 'json模板',
+  `comments` varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '注释',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='datax插件信息';
+) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = 'datax插件信息' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of datax_plugin
 -- ----------------------------
-INSERT INTO `datax_plugin` VALUES ('1', 'reader', 'streamreader', '', '内存读取');
-INSERT INTO `datax_plugin` VALUES ('2', 'writer', 'streamwriter', null, '内存写');
-INSERT INTO `datax_plugin` VALUES ('3', 'reader', 'mysqlreader', null, 'mysql读取');
-INSERT INTO `datax_plugin` VALUES ('4', 'writer', 'mysqlwriter', null, 'myysql写');
-INSERT INTO `datax_plugin` VALUES ('5', 'reader', 'oraclereader', null, 'oracle读取');
-INSERT INTO `datax_plugin` VALUES ('6', 'reader', 'sdfasdf', 'test', 'test');
-INSERT INTO `datax_plugin` VALUES ('12', 'reader', 'test', null, null);
+INSERT INTO `datax_plugin` VALUES (1, 'reader', 'streamreader', '', '内存读取');
+INSERT INTO `datax_plugin` VALUES (2, 'writer', 'streamwriter', NULL, '内存写');
+INSERT INTO `datax_plugin` VALUES (3, 'reader', 'mysqlreader', NULL, 'mysql读取');
+INSERT INTO `datax_plugin` VALUES (4, 'writer', 'mysqlwriter', NULL, 'myysql写');
+INSERT INTO `datax_plugin` VALUES (5, 'reader', 'oraclereader', NULL, 'oracle读取');
+INSERT INTO `datax_plugin` VALUES (6, 'reader', 'sdfasdf', 'test', 'test');
+INSERT INTO `datax_plugin` VALUES (12, 'reader', 'test', NULL, NULL);
 
 -- ----------------------------
 -- Table structure for job_config
 -- ----------------------------
 DROP TABLE IF EXISTS `job_config`;
-CREATE TABLE `job_config` (
+CREATE TABLE `job_config`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL COMMENT '作业名',
-  `job_group` varchar(255) DEFAULT NULL COMMENT '分组',
-  `config_json` varchar(5000) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL COMMENT '作业描述信息',
-  `label` varchar(255) DEFAULT NULL COMMENT '标签（读插件、写插件)',
-  `update_date` timestamp NULL DEFAULT NULL,
-  `status` int(1) DEFAULT '1',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '作业名',
+  `job_group` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '分组',
+  `config_json` varchar(5000) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '作业描述信息',
+  `label` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '标签（读插件、写插件)',
+  `update_date` timestamp(0) DEFAULT NULL,
+  `status` int(1) DEFAULT 1,
   `create_by` int(11) DEFAULT NULL,
-  `create_date` timestamp NULL DEFAULT NULL,
+  `create_date` timestamp(0) DEFAULT NULL,
   `update_by` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='作业配置表';
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '作业配置表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of job_config
 -- ----------------------------
-INSERT INTO `job_config` VALUES ('1', null, 'test', 'test', null, null, null, '2019-06-17 21:10:31', '0', null, '2019-06-17 21:10:31', null);
-INSERT INTO `job_config` VALUES ('2', null, 'testt', 'test', '{\n	\"abc\": \"def\"\n}', null, null, '2019-06-17 22:22:03', '0', null, '2019-06-17 21:14:30', null);
-INSERT INTO `job_config` VALUES ('3', null, '示例', 'test', '{\n  \"job\": {\n    \"setting\": {\n      \"speed\": {\n        \"channel\": 3\n      },\n      \"errorLimit\": {\n        \"record\": 0,\n        \"percentage\": 0.02\n      }\n    },\n    \"content\": [\n      {\n        \"reader\": {\n          \"name\": \"mysqlreader\",\n          \"parameter\": {\n            \"username\": \"root\",\n            \"password\": \"root\",\n            \"column\": [\n              \"id\",\n              \"name\"\n            ],\n            \"splitPk\": \"db_id\",\n            \"connection\": [\n              {\n                \"table\": [\n                  \"table\"\n                ],\n                \"jdbcUrl\": [\n                  \"jdbc:mysql://127.0.0.1:3306/database\"\n                ]\n              }\n            ]\n          }\n        },\n        \"writer\": {\n          \"name\": \"streamwriter\",\n          \"parameter\": {\n            \"print\": true\n          }\n        }\n      }\n    ]\n  }\n}', null, null, '2019-06-17 22:28:21', '1', null, '2019-06-17 22:05:31', null);
-INSERT INTO `job_config` VALUES ('4', null, 'test', '123', '{\n  \"job\": {\n    \"setting\": {\n      \"speed\": {\n        \"channel\": 3\n      },\n      \"errorLimit\": {\n        \"record\": 0,\n        \"percentage\": 0.02\n      }\n    },\n    \"content\": [\n      {\n        \"reader\": {\n          \"name\": \"mysqlreader\",\n          \"parameter\": {\n            \"username\": \"root\",\n            \"password\": \"root\",\n            \"column\": [\n              \"id\",\n              \"name\"\n            ],\n            \"splitPk\": \"db_id\",\n            \"connection\": [\n              {\n                \"table\": [\n                  \"table\"\n                ],\n                \"jdbcUrl\": [\n                  \"jdbc:mysql://127.0.0.1:3306/database\"\n                ]\n              }\n            ]\n          }\n        },\n        \"writer\": {\n          \"name\": \"streamwriter\",\n          \"parameter\": {\n            \"print\": true\n          }\n        }\n      }\n    ]\n  }\n}', null, null, '2019-06-17 22:28:31', '1', null, '2019-06-17 22:28:31', null);
+INSERT INTO `job_config` VALUES (1, NULL, 'test', 'test', NULL, NULL, NULL, '2019-06-17 21:10:31', 0, NULL, '2019-06-17 21:10:31', NULL);
+INSERT INTO `job_config` VALUES (2, NULL, 'testt', 'test', '{\n	\"abc\": \"def\"\n}', NULL, NULL, '2019-06-17 22:22:03', 0, NULL, '2019-06-17 21:14:30', NULL);
+INSERT INTO `job_config` VALUES (3, NULL, '示例', 'test', '{\n  \"job\": {\n    \"setting\": {\n      \"speed\": {\n        \"channel\": 3\n      },\n      \"errorLimit\": {\n        \"record\": 0,\n        \"percentage\": 0.02\n      }\n    },\n    \"content\": [\n      {\n        \"reader\": {\n          \"name\": \"mysqlreader\",\n          \"parameter\": {\n            \"username\": \"root\",\n            \"password\": \"root\",\n            \"column\": [\n              \"id\",\n              \"name\"\n            ],\n            \"splitPk\": \"db_id\",\n            \"connection\": [\n              {\n                \"table\": [\n                  \"table\"\n                ],\n                \"jdbcUrl\": [\n                  \"jdbc:mysql://127.0.0.1:3306/database\"\n                ]\n              }\n            ]\n          }\n        },\n        \"writer\": {\n          \"name\": \"streamwriter\",\n          \"parameter\": {\n            \"print\": true\n          }\n        }\n      }\n    ]\n  }\n}', NULL, NULL, '2019-06-17 22:28:21', 1, NULL, '2019-06-17 22:05:31', NULL);
+INSERT INTO `job_config` VALUES (4, NULL, 'test', '123', '{\n  \"job\": {\n    \"setting\": {\n      \"speed\": {\n        \"channel\": 3\n      },\n      \"errorLimit\": {\n        \"record\": 0,\n        \"percentage\": 0.02\n      }\n    },\n    \"content\": [\n      {\n        \"reader\": {\n          \"name\": \"mysqlreader\",\n          \"parameter\": {\n            \"username\": \"root\",\n            \"password\": \"root\",\n            \"column\": [\n              \"id\",\n              \"name\"\n            ],\n            \"splitPk\": \"db_id\",\n            \"connection\": [\n              {\n                \"table\": [\n                  \"table\"\n                ],\n                \"jdbcUrl\": [\n                  \"jdbc:mysql://127.0.0.1:3306/database\"\n                ]\n              }\n            ]\n          }\n        },\n        \"writer\": {\n          \"name\": \"streamwriter\",\n          \"parameter\": {\n            \"print\": true\n          }\n        }\n      }\n    ]\n  }\n}', NULL, NULL, '2019-06-17 22:28:31', 1, NULL, '2019-06-17 22:28:31', NULL);
+INSERT INTO `job_config` VALUES (5, NULL, NULL, NULL, '{}', NULL, NULL, '2019-06-25 10:28:51', 1, NULL, '2019-06-25 10:28:51', NULL);
+INSERT INTO `job_config` VALUES (6, NULL, NULL, NULL, '{}', NULL, NULL, '2019-06-25 10:47:59', 1, NULL, '2019-06-25 10:47:59', NULL);
 
 -- ----------------------------
 -- Table structure for job_log
 -- ----------------------------
 DROP TABLE IF EXISTS `job_log`;
-CREATE TABLE `job_log` (
+CREATE TABLE `job_log`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `job_id` bigint(20) NOT NULL COMMENT '抽取任务，主键ID',
-  `log_file_path` varchar(255) DEFAULT NULL COMMENT '日志文件路径',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='抽取日志记录表';
+  `log_file_path` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '日志文件路径',
+  `update_date` datetime(0) DEFAULT NULL,
+  `status` int(1) DEFAULT 1,
+  `create_by` int(11) DEFAULT NULL,
+  `create_date` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0),
+  `update_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '抽取日志记录表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of job_log
 -- ----------------------------
+INSERT INTO `job_log` VALUES (1, 1, '/data/applogs/datax-web/1_1561638145701', NULL, 1, NULL, '2019-06-27 08:22:26', NULL);
+INSERT INTO `job_log` VALUES (2, 1, '/data/applogs/datax-web/1_1561638359975.log', NULL, 1, NULL, '2019-06-27 08:26:00', NULL);
+INSERT INTO `job_log` VALUES (3, 1, '/data/applogs/datax-web/1_1561638760584.log', NULL, 1, NULL, '2019-06-27 08:32:41', NULL);
+
+SET FOREIGN_KEY_CHECKS = 1;

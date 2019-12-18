b0190b289f090a0a41f21fdf33e8213c3c34ee12
weiye <jingwk@greatld.com>
2019-12-18 14:04:45 +0800

    job_group 添加默认值

--- doc/db/datax_web.sql
+++ doc/db/datax_web.sql
@@ -58,6 +58,10 @@ CREATE TABLE `job_group`  (
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
+-- ----------------------------
+-- Records of job_group
+-- ----------------------------
+INSERT INTO `job_group` VALUES (1, 'datax-executor', 'datax执行器', 1, 0, NULL);
 
 -- ----------------------------
 -- Table structure for job_info

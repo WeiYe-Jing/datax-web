1a2b2cd68a814f7bcf24fa84ba1e3720bb417c7c
water <672684719@qq.com>
2020-03-28 22:59:36 +0800

    update:优化代码

--- datax-rpc/src/main/java/com/wugui/datax/rpc/util/IpUtil.java
+++ datax-rpc/src/main/java/com/wugui/datax/rpc/util/IpUtil.java
@@ -24,7 +24,6 @@ public class IpUtil {
     private static final Pattern IP_PATTERN = Pattern.compile("\\d{1,3}(\\.\\d{1,3}){3,5}$");
 
 
-
     private static volatile InetAddress LOCAL_ADDRESS = null;
 
     // ---------------------- valid ----------------------
@@ -57,11 +56,10 @@ public class IpUtil {
             return false;
         }
         String name = address.getHostAddress();
-        boolean result = (name != null
+        return (name != null
                 && IP_PATTERN.matcher(name).matches()
                 && !ANYHOST_VALUE.equals(name)
                 && !LOCALHOST_VALUE.equals(name));
-        return result;
     }
 
 
@@ -125,7 +123,7 @@ public class IpUtil {
                             InetAddress addressItem = toValidAddress(addresses.nextElement());
                             if (addressItem != null) {
                                 try {
-                                    if(addressItem.isReachable(100)){
+                                    if (addressItem.isReachable(100)) {
                                         return addressItem;
                                     }
                                 } catch (IOException e) {
@@ -168,7 +166,7 @@ public class IpUtil {
      *
      * @return String
      */
-    public static String getIp(){
+    public static String getIp() {
         return getLocalAddress().getHostAddress();
     }
 
@@ -178,19 +176,19 @@ public class IpUtil {
      * @param port
      * @return String
      */
-    public static String getIpPort(int port){
+    public static String getIpPort(int port) {
         String ip = getIp();
         return getIpPort(ip, port);
     }
 
-    public static String getIpPort(String ip, int port){
-        if (ip==null) {
+    public static String getIpPort(String ip, int port) {
+        if (ip == null) {
             return null;
         }
         return ip.concat(":").concat(String.valueOf(port));
     }
 
-    public static Object[] parseIpPort(String address){
+    public static Object[] parseIpPort(String address) {
         String[] array = address.split(":");
 
         String host = array[0];

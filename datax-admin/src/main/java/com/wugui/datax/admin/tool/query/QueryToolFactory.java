3bd39b4998e2ac9a98c7af5128f9c588e16bf2a2
weiye <jingwk@greatld.com>
2020-03-30 12:19:24 +0800

    clickhouse数据源页面更新

--- datax-admin/src/main/java/com/wugui/datax/admin/tool/query/QueryToolFactory.java
+++ datax-admin/src/main/java/com/wugui/datax/admin/tool/query/QueryToolFactory.java
@@ -29,7 +29,7 @@ public class QueryToolFactory {
             return getSqlserverQueryToolInstance(jobDatasource);
         }else if (JdbcConstants.HIVE.equals(datasource)) {
             return getHiveQueryToolInstance(jobDatasource);
-        } else if (JdbcConstants.CLICKHOUSE.equals("clickhouse")) {
+        } else if (JdbcConstants.CLICKHOUSE.equals(jobDatasource)) {
 
             return getClickHouseQueryToolInstance(jobDatasource);
 

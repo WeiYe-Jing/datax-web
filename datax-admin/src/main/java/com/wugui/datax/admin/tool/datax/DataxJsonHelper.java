2b59dfb979644a76d2943eac000fe2f227503f88
yuanyj <yuanhotel@qq.com>
2020-04-15 18:37:10 +0800

    修改hbase构建writer时设了错误的表名

--- datax-admin/src/main/java/com/wugui/datax/admin/tool/datax/DataxJsonHelper.java
+++ datax-admin/src/main/java/com/wugui/datax/admin/tool/datax/DataxJsonHelper.java
@@ -301,7 +301,7 @@ public class DataxJsonHelper implements DataxJsonInterface {
         }
         dataxHbasePojo.setColumns(columns);
         dataxHbasePojo.setWriterHbaseConfig(writerDatasource.getZkAdress());
-        dataxHbasePojo.setWriterTable(readerTables);
+        dataxHbasePojo.setWriterTable(writerTables);
         dataxHbasePojo.setWriterVersionColumn(hbaseWriterDto.getWriterVersionColumn());
         dataxHbasePojo.setWriterRowkeyColumn(hbaseWriterDto.getWriterRowkeyColumn());
         dataxHbasePojo.setWriterMode(hbaseWriterDto.getWriterMode());

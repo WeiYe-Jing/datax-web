10bb1db82a624d0accb0bd4ee3449678a659f535
weiye <1058304821@qq.com>
2020-07-30 08:43:07 +0800

    解决oracle下无法获取schema，视图名，视图字段 #250

--- datax-admin/src/main/java/com/wugui/datax/admin/tool/meta/OracleDatabaseMeta.java
+++ datax-admin/src/main/java/com/wugui/datax/admin/tool/meta/OracleDatabaseMeta.java
@@ -52,7 +52,10 @@ public class OracleDatabaseMeta extends BaseDatabaseMeta implements DatabaseInte
 
     @Override
     public String getSQLQueryTables(String... tableSchema) {
-        return "select owner||'.'||table_name from all_tables where owner='" + tableSchema[0] + "'  order by table_name";
+        return "select owner||'.'||table_name from all_tables where owner='" + tableSchema[0] + "' " +
+                "union " +
+                "select owner||'.'||view_name as table_name from all_views where owner='" + tableSchema[0] + "'" +
+                "order by table_name";
     }
 
     @Override

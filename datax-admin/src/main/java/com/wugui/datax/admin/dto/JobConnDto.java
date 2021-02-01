38cb200e3e445c99f8b12698794e8698aa0bdea6
liuyuanhui <yuanhui0110>
2021-02-01 17:33:43 +0800

    漏提交代码

---
+++ datax-admin/src/main/java/com/wugui/datax/admin/dto/JobConnDto.java
@@ -0,0 +1,24 @@
+package com.wugui.datax.admin.dto;
+
+public class JobConnDto {
+
+    private Integer sourceId;
+
+    private Integer targetId;
+
+    public Integer getSourceId() {
+        return sourceId;
+    }
+
+    public void setSourceId(Integer sourceId) {
+        this.sourceId = sourceId;
+    }
+
+    public Integer getTargetId() {
+        return targetId;
+    }
+
+    public void setTargetId(Integer targetId) {
+        this.targetId = targetId;
+    }
+}

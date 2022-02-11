ccb0a9936ae9479b8cc2b58fdb872d215f37647a
SharkDoWork <37807297+SharkDoWork@users.noreply.github.com>
2022-02-11 15:37:50 +0800

    Update datax_web.sql

--- bin/db/datax_web.sql
+++ bin/db/datax_web.sql
@@ -335,6 +335,10 @@ ADD COLUMN `inc_start_id` VARCHAR(20) NULL COMMENT '增量初始id' AFTER `prima
 ADD COLUMN `increment_type` TINYINT(4) NULL COMMENT '增量类型' AFTER `inc_start_id`,
 ADD COLUMN `datasource_id` BIGINT(11) NULL COMMENT '数据源id' AFTER `increment_type`;
 
+-- ----------------------------
+-- Table structure for job_project
+-- ----------------------------
+DROP TABLE IF EXISTS `job_project`;
 CREATE TABLE `job_project`  (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'key',
   `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT 'project name',
